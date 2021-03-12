c3c29bf1920abb14142c9686fc3f12d712b13df4
Ole Brodersen <brodersen.ole@gmail.com>
2021-03-12 08:53:38 +0100

    ADD: First and probably final version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# change_display_extend_win10
+# Windows 10 - change display settings with a double-click
 simple bat files to change monitor settings.
+
+Problem was that I had to switch monitor settings from my private laptop to extend to the one external monitor I had on my desk. During working hours I had that one connected to my  business laptop. And I did not want to always <code>right click on desktop - change display settings - extend to second monitor - ok</code> or use <code>Win-P - click</code>.
+
+So I came up with this double-click bat file...
+
+
---
+++ internal_monitor.bat
@@ -0,0 +1,8 @@
+REM C:\Windows\System32\DisplaySwitch.exe opens setting like Windows-Key + P
+REM C:\Windows\System32\DisplaySwitch.exe /external opens external device like beamer
+REM C:\Windows\System32\DisplaySwitch.exe /internal switches to internal monitor (e.g. laptop) 
+REM C:\Windows\System32\DisplaySwitch.exe /extend extends display to other monitors (with laptop to external monitor)
+REM C:\Windows\System32\DisplaySwitch.exe /clone clones display to second screen
+REM 
+REM --- switching to internal screen ---
+C:\Windows\System32\DisplaySwitch.exe /internal
